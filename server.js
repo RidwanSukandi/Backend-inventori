cd75d8ac41fcf9d3bef9cb7f93fe86df63c0dd5d
RidwanSukandi <ridwansukandi17@gmail.com>
2023-06-17 20:33:47 +0700

    fix server

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://inventory-fawn.vercel.app/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader(
     "Access-Control-Allow-Methods",
